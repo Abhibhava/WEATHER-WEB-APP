6a413a28bd14f0a223ebf646118987331ec32e0c
Abhibhava <90981237+Abhibhava@users.noreply.github.com>
2023-08-29 21:00:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const apiKey = "bf2863de1132beff4973ff179fbfa7c0";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+
+const formEl = document.querySelector("form");
+
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+}
+)
+
+async function getWeatherData(cityValue){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
+
+        if(!response.ok){
+            throw new Error("It is showing some error");
+        }
+
+        const data = await response.json(); // converted into json, as the raw response cannot be used. 
+        
+        // filling from the response we got
+        const temperature = Math.round(data.main.temp);
+
+        const description = data.weather[0].description;
+
+        const icon = data.weather[0].icon;
+
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}°C`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind speed: ${data.wind.speed} m/s`,
+        ]
+
+
+
+        // now, actually filling in the weatherDataEl, as this div contains all the inner parts
+
+        weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`
+
+        weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`
+
+        weatherDataEl.querySelector(".description").textContent = `${description}`
+
+        weatherDataEl.querySelector(".details").innerHTML = details.map((detail) => 
+        `<div>${detail}</div>`).join("");
+    }
+    
+    
+    catch(error){
+        weatherDataEl.querySelector(".description").textContent = "An error happened";
+    }
+}
